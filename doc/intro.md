6ac3d377e652c196d11df0649f4434d26f3ce835
Marcin Gajewski <code@gajewsky.it>
2020-03-19 18:03:29 +0100

    Init commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to revolut-csv-parser
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/revolut_csv_parser/core_test.clj
@@ -0,0 +1,7 @@
+(ns revolut-csv-parser.core-test
+  (:require [clojure.test :refer :all]
+            [revolut-csv-parser.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
